649d16b6d7ec46e60a2af958e1a22bf1d325f9db
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-08 20:51:00 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,11 +1,23 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+// jest.mock('bcrypt', () => ({
+//   async hash (): Promise<string> {
+//     return await new Promise(resolve => { resolve('hashed_value') })
+//   }
+// }))
+
 jest.mock('bcrypt', () => {
+  const hash = async (value: string, salt: number): Promise<string> => {
+    return (
+      await new Promise((resolve) => {
+        resolve('hashed_value')
+      })
+    )
+  }
+
   return {
-    async hash (value: string, salt: number): Promise<string> {
-      return await new Promise(resolve => { resolve('hashed_value') })
-    }
+    hash
   }
 })
 
@@ -26,5 +38,15 @@ describe('Bcrypt Adapter', () => {
     const sut = makeSut()
     const hashedValue = await sut.encrypt('any_value')
     expect(hashedValue).toBe('hashed_value')
+  })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => { reject(new Error()) })
+    })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })

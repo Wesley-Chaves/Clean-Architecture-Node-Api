8ece175485b66be65d204367ec867bd6581fce25
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-08 22:48:53 -0300

    feat: ensure AccountMongoRepository returns an account on sucess

---
+++ src/infra/database/mongodb/account-repository/account.ts
@@ -0,0 +1,13 @@
+import { type AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { type IAccountModel } from '../../../../domain/models/account'
+import { type IAddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: IAddAccountModel): Promise<IAccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account = await accountCollection.findOne({ _id: result.insertedId })
+    return MongoHelper.map(account)
+  }
+}
--- src/infra/database/mongodb/helpers/mongo-helper.ts
+++ src/infra/database/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, type Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -10,5 +10,14 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map (dataFromSearchDb: any): any {
+    const { _id, ...obj } = dataFromSearchDb
+    return Object.assign({}, { id: _id }, obj)
   }
 }

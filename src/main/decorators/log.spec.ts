67f92292a2ab58ba1c0755c8920ef555caa4eabf
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-10 20:33:11 -0300

    test: ensure LogControllerDecorator calls controller with correct values

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,31 @@
+import { LogControllerDecorator } from './log'
+import { HttpRequest, HttpResponse, IController } from '../../presentation/protocols'
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    class ControllerStub implements IController {
+      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+        const httpResponse: HttpResponse = {
+          statusCode: 200,
+          body: {
+            name: 'Wesley'
+          }
+        }
+        return await new Promise((resolve) => { resolve(httpResponse) })
+      }
+    }
+    const controllerStub: IController = new ControllerStub()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const sut = new LogControllerDecorator(controllerStub)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})

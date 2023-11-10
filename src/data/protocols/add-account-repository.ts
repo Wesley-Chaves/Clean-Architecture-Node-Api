b2a38d61a74d901dbd208ca6997b9a9dbecd1370
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-10 18:29:54 -0300

    refactor: fix bug interface

--- src/data/protocols/add-account-repository.ts
+++ src/data/protocols/add-account-repository.ts
@@ -1,7 +1,5 @@
-import { type IAccountModel, type IAddAccount, type IAddAccountModel } from '../usecases/add-account/db-add-account-protocols'
+import { type IAccountModel, type IAddAccountModel } from '../usecases/add-account/db-add-account-protocols'
 
-export class AddAccountRepository implements IAddAccount {
-  async add (account: IAddAccountModel): Promise<IAccountModel> {
-    return await new Promise((resolve) => { resolve(null) })
-  }
+export interface AddAccountRepository {
+  add: (account: IAddAccountModel) => Promise<IAccountModel>
 }

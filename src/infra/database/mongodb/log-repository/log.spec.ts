754eee7b62268e98caf89e63c6692f09e7fd3bf2
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-11 16:45:39 -0300

    test: ensure LogMongoRepository creates an error log on sucess

---
+++ src/infra/database/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,32 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+describe('Log Error Mongo Repository', () => {
+  let errorCollection: Collection
+
+  const clearLogs = async (): Promise<void> => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  }
+
+  beforeAll(async () => {
+    await MongoHelper.connect('mongodb://localhost:27017')
+    await clearLogs()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await clearLogs()
+  })
+
+  test('Should create an error log on sucess', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
